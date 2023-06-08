88f41ce6caf36179b08c028210902ff3d241db39
Farrongua <70178844+Farrongua@users.noreply.github.com>
2023-06-08 18:56:44 +0300

    Added source files

---
+++ Lab1/views/DescriptionView.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+
+<template>
+    <div style="display: flex; flex-direction: column; justify-content: center; gap: 2px; align-items: center">
+        <img src="../logo/UIP-logo.jpg" alt="img" width="51" height="51">
+        <p>This is app for calculation</p>
+    </div>
+   
+</template>
+
+<style scoped>
+
+</style>
+
